4279fc66bdb00343bc25fa4ef486b1d76193223f
tenzee theo <99639473+TenzeeTheo@users.noreply.github.com>
2024-05-24 00:48:21 +1000

    Update setting.php

--- includes/setting.php
+++ includes/setting.php
@@ -1,9 +1,2 @@
-<?php
-// 1. Declare connection parameter variables
-
-$servername = "feenix-mariadb.swin.edu.au"; // your  servername
-$username = "s105187238"; // your user name
-$password = "101096"; // your password (date of birth ddmmyy unless changed)
-$dbname = "s105187238_db"; // your database
 
 
